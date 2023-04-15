fb2616e8bb76c345988c511f5aa27b317af06212
Mis311 <ley.hanamura@gmail.com>
2023-04-16 03:58:51 +0900

    nft arts add

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,27 +1,80 @@
 import React from "react";
 import ProjectCard from "../components/cards/ProjectCard";
 
+
 const projectData = [
   {
     title: "Project 1",
     description: "This is a description of project 1.",
-    image: "https://via.placeholder.com/300",
+    image: "/nfts/cow1.png",
     width: 300,
     height: 300,
   },
   {
-    title: "Project 2",
-    description: "This is a description of project 2.",
-    image: "https://via.placeholder.com/300",
+    title: "Project 1",
+    description: "This is a description of project 1.",
+    image: "/nfts/cow2.png",
     width: 300,
     height: 300,
   },
+  {
+    title: "Project 1",
+    description: "This is a description of project 1.",
+    image: "/nfts/cow3.png",
+    width: 300,
+    height: 300,
+  },
+  {
+    title: "Project 1",
+    description: "This is a description of project 1.",
+    image: "/nfts/cow4.png",
+    width: 300,
+    height: 300,
+  },
+  {
+    title: "Project 1",
+    description: "This is a description of project 1.",
+    image: "/nfts/cow1.png",
+    width: 300,
+    height: 300,
+  },
+
+  {
+    title: "Project 1",
+    description: "This is a description of project 1.",
+    image: "/nfts/cow1.png",
+    width: 300,
+    height: 300,
+  },
+  {
+    title: "Project 1",
+    description: "This is a description of project 1.",
+    image: "/nfts/cow1.png",
+    width: 300,
+    height: 300,
+  },
+  {
+    title: "Project 1",
+    description: "This is a description of project 1.",
+    image: "/nfts/cow1.png",
+    width: 300,
+    height: 300,
+  },
+  {
+    title: "Project 1",
+    description: "This is a description of project 1.",
+    image: "/nfts/cow1.png",
+    width: 300,
+    height: 300,
+  },
+
 ];
 
 function Marketplace() {
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-semibold mb-4">Marketplace</h1>
+     
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {projectData.map((project, index) => (
           <ProjectCard key={index} projectData={project} layout="vertical" />
