bd16c8d1dff08ee4741395ac85e7b0aa64c96ff7
Mis311 <ley.hanamura@gmail.com>
2023-04-16 05:11:33 +0900

    card finished

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ProjectCard from "../components/cards/ProjectCard";
-
+import TalentCard from "../components/cards/TalentCard";
 
 const projectData = [
   {
@@ -67,14 +67,73 @@ const projectData = [
     width: 300,
     height: 300,
   },
+];
 
+const jobData = [
+  {
+    title: "Web3 Developer",
+    description: "Join our team to develop and deploy smart contracts.",
+    image: "/nfts/cow1.png",
+    payment: "Paid",
+    proofOfWork: "No",
+  },
+  {
+    title: "Marketing Specialist",
+    description: "Help promote our NFT farm and grow our community.",
+    image: "/nfts/cow1.png",
+    payment: "Paid",
+    proofOfWork: "No",
+  },
+  {
+    title: "Content Creator",
+    description: "Create engaging content for our NFT project.",
+    image: "/nfts/cow1.png",
+    payment: "Non-Paid",
+    proofOfWork: "Yes",
+  },
+  {
+    title: "Social Media Manager",
+    description:
+      "Manage our social media accounts and engage with the community.",
+    image: "/nfts/cow1.png",
+    payment: "Paid",
+    proofOfWork: "No",
+  },
+  {
+    title: "Graphic Designer",
+    description: "Design promotional materials and NFTs for our project.",
+    image: "/nfts/cow1.png",
+    payment: "Non-Paid",
+    proofOfWork: "Yes",
+  },
+  {
+    title: "Community Manager",
+    description: "Grow and manage our NFT project's community.",
+    image: "/nfts/cow1.png",
+    payment: "Paid",
+    proofOfWork: "No",
+  },
+  {
+    title: "Partnership Manager",
+    description: "Develop partnerships with other NFT projects and platforms.",
+    image: "/nfts/cow1.png",
+    payment: "Non-Paid",
+    proofOfWork: "Yes",
+  },
+  {
+    title: "UI/UX Designer",
+    description: "Design and improve the user experience on our platform.",
+    image: "/nfts/cow1.png",
+    payment: "Paid",
+    proofOfWork: "No",
+  },
 ];
 
 function Marketplace() {
   return (
     <div className="container mx-auto p-24">
       <h1 className="text-3xl font-semibold mb-6">Project NFT Top Sales</h1>
-     
+
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {projectData.map((project, index) => (
           <ProjectCard key={index} projectData={project} layout="vertical" />
@@ -82,8 +141,8 @@ function Marketplace() {
       </div>
       <div className="mt-8">
         <h2 className="text-2xl font-semibold mb-4">Job Postings</h2>
-        {projectData.map((project, index) => (
-          <ProjectCard key={index} projectData={project} layout="horizontal" />
+        {jobData.map((job, index) => (
+          <TalentCard key={index} jobData={job} layout="horizontal" />
         ))}
       </div>
     </div>
